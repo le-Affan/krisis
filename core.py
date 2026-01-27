f72223549f631eb94644962059387f8c5d72562c
le-Affan <shaikhaffan1234@gmail.com>
2026-01-28 00:33:44 +0530

    Delayed Outcome Recording Function Added

--- core.py
+++ core.py
@@ -4,21 +4,18 @@ import time
 
 
 # In-Memory state
-
 models = {}
 requests = {}
 outcomes = {}
 
 
 # model registration function
-
 def register_models(model_a, model_b):
     models["A"] = model_a
     models["B"] = model_b
 
 
 #  request routing function
-
 def route_request(X, probability_split):
 
     # Generate a unique request ID and timestamp
@@ -43,3 +40,12 @@ def route_request(X, probability_split):
     prediction = models[selected_model](X)
 
     return prediction, request_id
+
+
+# delayed outcome recording function
+def record_delayed_outcome(request_id, outcome):
+
+    if request_id in requests:
+        outcomes[request_id] = outcome
+    else:
+        raise ValueError("Request ID not found")

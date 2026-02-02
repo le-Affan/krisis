c1ab4db2a435bca13bdf9f9803cea91fe169120f
le-Affan <shaikhaffan1234@gmail.com>
2026-02-02 21:26:38 +0530

    statistics.py refactored. compute_statistics function is now an orchestrator of multiple functions each performing one pure task

--- src/statistics.py
+++ src/statistics.py
@@ -2,6 +2,51 @@ import numpy as np
 from scipy import stats
 import math
 
+def check_minimum_sample_size(n_A,n_B,min_size):
+    if (
+        n_A < min_size or n_B < min_size
+    ):  # ensures enough data for variance calculation
+        return False
+    else: 
+        return True
+
+def calculate_welch_test(outcomes_1,outcomes_2):
+    mean_A = np.mean(outcomes_1)
+    mean_B = np.mean(outcomes_2)
+
+    var_A = np.var(outcomes_1, ddof=1)
+    var_B = np.var(outcomes_2, ddof=1)
+
+    n_A = len(outcomes_1)
+    n_B = len(outcomes_2)
+
+    delta = mean_B - mean_A
+
+    # Standard error (Welch)
+    se = math.sqrt(var_A / n_A + var_B / n_B)
+
+    if var_A == 0 and var_B == 0:
+        return [mean_A, mean_B, delta, 0.0, None, n_A, n_B]
+
+    # Degrees of freedom (Welch–Satterthwaite)
+    df = (var_A / n_A + var_B / n_B) ** 2 / (
+        (var_A**2) / (n_A**2 * (n_A - 1)) + (var_B**2) / (n_B**2 * (n_B - 1))
+    )
+
+    return [mean_A, mean_B, delta, se, df, n_A, n_B]
+
+def calculate_confidence_interval(delta,se,df,confidence_level):
+
+    if se == 0:
+        return (delta, delta)
+    
+    alpha = 1 -confidence_level
+    t_crit = stats.t.ppf(1 - alpha / 2, df)
+
+    lower = delta - t_crit * se
+    upper = delta + t_crit * se
+
+    return (lower, upper)
 
 def compute_statistics(outcomes_1, outcomes_2):
     """
@@ -33,38 +78,13 @@ def compute_statistics(outcomes_1, outcomes_2):
     - Outcomes are continuous and approximately normally distributed.
     - Samples are independent between variants.
     """
-    if (
-        len(outcomes_1) < 2 or len(outcomes_2) < 2
-    ):  # ensures enough data for variance calculation
+    if not check_minimum_sample_size(len(outcomes_1), len(outcomes_2), 2):
         return None
 
-    mean_A = np.mean(outcomes_1)
-    mean_B = np.mean(outcomes_2)
+    mean_A, mean_B, delta, se, df, n_A, n_B = calculate_welch_test(
+        outcomes_1, outcomes_2
+        )
+    
+    ci_lower, ci_upper = calculate_confidence_interval(delta, se, df, 0.95)
 
-    var_A = np.var(outcomes_1, ddof=1)
-    var_B = np.var(outcomes_2, ddof=1)
-
-    n_A = len(outcomes_1)
-    n_B = len(outcomes_2)
-
-    delta = mean_B - mean_A
-
-    # Both groups have zero variance; return delta as 0 and CI as (0,0)
-    if var_A == 0 and var_B == 0:
-        return [mean_A, mean_B, delta, (0.0, 0.0), n_A, n_B]
-
-    # Standard error (Welch)
-    se = math.sqrt(var_A / n_A + var_B / n_B)
-
-    # Degrees of freedom (Welch–Satterthwaite)
-    df = (var_A / n_A + var_B / n_B) ** 2 / (
-        (var_A**2) / (n_A**2 * (n_A - 1)) + (var_B**2) / (n_B**2 * (n_B - 1))
-    )
-
-    # 95% CI
-    t_crit = stats.t.ppf(0.975, df)
-
-    lower = delta - t_crit * se
-    upper = delta + t_crit * se
-
-    return [mean_A, mean_B, delta, (lower, upper), n_A, n_B]
+    return [mean_A, mean_B, delta, (ci_lower, ci_upper), n_A, n_B]
